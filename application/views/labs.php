786a9424cafc9cb0988a69e10ee62fa2601a3a3c
Sayed Navid Azimi <navidjan.azimi132@gmail.com>
2025-03-21 00:59:57 +0430

    button list labs are working

--- application/views/labs.php
+++ application/views/labs.php
@@ -199,11 +199,19 @@
 								<?php
 								$teeths = explode(',', $lab['teeth']);
 								$teethName = '';
-								foreach ($teeths as $tooth) {
-									$info = $ci->tooth_by_id($tooth);
-									$teethName .= $info['name'];
-									$teethName .= $info['location'];
-									$teethName .= ',';
+								if (count($teeths) != 0) {
+
+									foreach ($teeths as $tooth) {
+										$info = $ci->tooth_by_id($tooth);
+										if (is_array($info) && count($info) != 0) {
+
+										$teethName .= $info['name'];
+										$teethName .= $info['location'];
+										}
+										$teethName .= ',';
+									}
+								}else{
+									$teethName = ',';
 								}
 								?>
 								<td><?= substr($teethName, 0, -1) ?></td>
@@ -226,40 +234,59 @@
 											<span
 												class="fa fa-user-circle-o"></span>
 										</a>
-										<?php if ($lab['first_try_status'] == 'p'): ?>
-											<a href="javascript:firstTry('<?= $lab['id'] ?>')"
-											   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-check-circle"></span></a>
-										<?php else: ?>
-											<a href="javascript:showTry('<?= $lab['id'] ?>', 'first')"
-											   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-eye"></span></a>
-										<?php endif; ?>
+										<?php if ($lab['init_date'] != ''): ?>
+											<?php if ($lab['first_try_status'] == 'p'): ?>
+												<a href="javascript:firstTry('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-check-circle"></span></a>
+											<?php else: ?>
+												<a href="javascript:showTry('<?= $lab['id'] ?>', 'first')"
+												   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-eye"></span></a>
+											<?php endif; ?>
 
-										<?php if ($lab['second_try_status'] == 'p'): ?>
-											<a href="javascript:secondTry('<?= $lab['id'] ?>')"
-											   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-check-circle"></span></a>
-										<?php else: ?>
-											<a href="javascript:showTry('<?= $lab['id'] ?>', 'second')"
-											   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-eye"></span></a>
-										<?php endif; ?>
-										<?php if ($lab['status'] == 'p'): ?>
-											<a href="javascript:finish('<?= $lab['id'] ?>')"
-											   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-check-circle"></span></a>
-										<?php else: ?>
-											<a href="javascript:showfinish('<?= $lab['id'] ?>')"
-											   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
-													class="fa fa-eye"></span></a>
-										<?php endif; ?>
-										<?php if ($lab['status'] != 'm'): ?>
-											<a href="javascript:payLab('<?= $lab['id'] ?>')"
-											   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light <?= ($lab['status'] != 'a') ? 'locked' : '' ?>"><span
-													class="fa fa-money"></span></a>
-										<?php endif; ?>
+											<?php if ($lab['second_try_status'] == 'p'): ?>
+												<a href="javascript:secondTry('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-check-circle"></span></a>
+											<?php else: ?>
+												<a href="javascript:showTry('<?= $lab['id'] ?>', 'second')"
+												   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-eye"></span></a>
+											<?php endif; ?>
 
+
+											<?php if ($lab['status'] == 'p'): ?>
+												<a href="javascript:finish('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-check-circle"></span></a>
+											<?php else: ?>
+												<a href="javascript:showfinish('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-eye"></span></a>
+											<?php endif; ?>
+
+											<?php if ($lab['install_time'] == ''): ?>
+												<a href="javascript:install('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light  <?= ($lab['status'] == 'p') ? 'locked' : '' ?>"><span
+														class="fa fa-tooth"></span></a>
+											<?php else: ?>
+												<a href="javascript:showinstall('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-primary rounded-pill btn-wave waves-effect waves-light"><span
+														class="fa fa-eye"></span></a>
+											<?php endif; ?>
+
+											<?php if ($lab['status'] != 'm'): ?>
+												<a href="javascript:payLab('<?= $lab['id'] ?>')"
+												   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light <?= ($lab['status'] != 'a' || is_null($lab['install_time'])) ? 'locked' : '' ?>"><span
+														class="fa fa-money"></span></a>
+											<?php endif; ?>
+
+										<?php else: ?>
+											<a href="javascript:init_lab('<?= $lab['id'] ?>')"
+											   class="btn btn-icon btn-outline-success rounded-pill btn-wave waves-effect waves-light"><span
+													class="fa fa-check-circle"></span></a>
+										<?php endif; ?>
 										<a href="javascript:print_lab('<?= $lab['id'] ?>')"
 										   class="btn btn-icon btn-outline-warning rounded-pill btn-wave waves-effect waves-light"><span
 												class="fa-solid fa-print fs-14"></span></a>
@@ -379,3 +406,288 @@
 		})
 	}
 </script>
+<script>
+	function print_lab(labId) {
+		window.open(`<?= base_url() ?>admin/print_lab/${labId}`, '_blank');
+	}
+</script>
+
+<script>
+	function firstTry(id){
+		$('#type_try').val('first');
+		$('#hiddenId').val(id);
+		$("#triesModal").modal('toggle');
+	}
+	function secondTry(id){
+		$('#type_try').val('second');
+		$('#hiddenId').val(id);
+		$("#triesModal").modal('toggle');
+	}
+
+	function finish(id){
+		$.ajax({
+			url: "<?= base_url('admin/finish_lab') ?>",
+			type: 'POST',
+			data: {
+				record: id
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				if (result['type'] == 'success') {
+					list_labs();
+					toastr["success"](result['alert']['text'], result['alert']['title'])
+				}else{
+					toastr["error"](result['messages'][step], result['title'])
+				}
+
+			}
+		})
+	}
+
+	function showTry(id, type){
+		$.ajax({
+			url: "<?= base_url('admin/show_try') ?>",
+			type: 'POST',
+			data: {
+				type: type,
+				record: id
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				if (result['type'] == 'success') {
+					$('#details_lab').show();
+					$('#details_lab').val(result['content']['message']);
+					$('#datetime_lab').val(result['content']['datetime']);
+					$("#showtriesModal").modal('toggle');
+				}else{
+					toastr["error"](result['messages'][step], result['title'])
+				}
+
+			}
+		})
+	}
+
+	function showfinish(id){
+		$.ajax({
+			url: "<?= base_url('admin/show_try') ?>",
+			type: 'POST',
+			data: {
+				type: 'finish',
+				record: id
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				if (result['type'] == 'success') {
+					$('#details_lab').hide();
+					$('#datetime_lab').val(result['content']['datetime']);
+					$("#showtriesModal").modal('toggle');
+				}else{
+					toastr["error"](result['messages'][step], result['title'])
+				}
+
+			}
+		})
+	}
+
+
+
+	function showinstall(id){
+		$.ajax({
+			url: "<?= base_url('admin/show_try') ?>",
+			type: 'POST',
+			data: {
+				type: 'install',
+				record: id
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				if (result['type'] == 'success') {
+					$('#details_lab').hide();
+					$('#datetime_lab').val(result['content']['datetime']);
+					$("#showtriesModal").modal('toggle');
+				}else{
+					toastr["error"](result['messages'][step], result['title'])
+				}
+
+			}
+		})
+	}
+
+
+	function install(id){
+		$.ajax({
+			url: "<?= base_url('admin/install_lab') ?>",
+			type: 'POST',
+			data: {
+				record: id
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				if (result['type'] == 'success') {
+					list_labs();
+					toastr["success"](result['alert']['text'], result['alert']['title'])
+				}else{
+					toastr["error"](result['messages'][step], result['title'])
+				}
+
+			}
+		})
+	}
+
+	function init_lab(id){
+		$('#init_lab_id').val(id);
+		$("#init_lab").modal('toggle');
+	}
+
+</script>
+
+
+<?php $ci = get_instance(); ?>
+
+<!-- Modal Call -->
+<div class="modal fade effect-scale" id="triesModal" role="dialog">
+	<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">
+					<!-- change language -->
+					<?= $ci->lang('desc') ?>
+				</h5>
+				<button class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="tryForm">
+					<div class="row">
+
+						<div class="col-sm-12 col-md-12">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('desc') ?></label>
+								<textarea type="text" name="remarks" id="callDetails1" class="form-control" rows="4"
+										  placeholder="<?= $ci->lang('desc') ?>"></textarea>
+							</div>
+						</div>
+						<input id="type_try" type="hidden" name="type" class="form-control">
+						<input id="hiddenId" type="hidden" name="slug" class="form-control">
+
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" data-bs-dismiss="modal">
+					<?= $ci->lang('cancel') ?>
+				</button>
+				<button class="btn btn-primary"
+						onclick="submitWithoutDatatable('tryForm', '<?= base_url() ?>admin/tryLab', 'labsTable', 'triesModal', list_labs)">
+					<?= $ci->lang('Submit') ?>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Modal Call End -->
+
+<!-- Modal Show -->
+<div class="modal fade effect-scale" id="showtriesModal" role="dialog">
+	<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">
+					<!-- change language -->
+					<?= $ci->lang('desc') ?>
+				</h5>
+				<button class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="">
+					<div class="row">
+
+						<div class="col-sm-12 col-md-12">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('date') ?></label>
+								<input type="text" id="datetime_lab" class="form-control" disabled>
+							</div>
+						</div>
+
+						<div class="col-sm-12 col-md-12">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('desc') ?></label>
+								<textarea type="text" name="remarks" id="details_lab" class="form-control" rows="4"
+										  placeholder="<?= $ci->lang('desc') ?>"></textarea>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" data-bs-dismiss="modal">
+					<?= $ci->lang('cancel') ?>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Modal Show End -->
+
+<!-- Modal Show -->
+<div class="modal fade effect-scale" id="init_lab" role="dialog">
+	<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">
+					<!-- change language -->
+					<?= $ci->lang('desc') ?>
+				</h5>
+				<button class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">×</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form id="init_lab_form">
+					<div class="row">
+
+						<div class="col-sm-12 col-md-6">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('date') ?></label>
+								<input type="hidden" name="slug" id="init_lab_id">
+								<input data-jdp type="text" id="deliveryDate_edit" name="give_date"
+									   class="form-control">
+							</div>
+						</div>
+
+						<div class="col-sm-12 col-md-6">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('hour') ?></label>
+								<input type="time" name="hour" list="from_times" class="form-control">
+							</div>
+						</div>
+
+						<div class="col-sm-12 col-md-12">
+							<div class="form-group">
+								<label class="form-label"><?= $ci->lang('desc') ?></label>
+								<textarea type="text" name="remarks" id="details_lab" class="form-control" rows="4"
+										  placeholder="<?= $ci->lang('desc') ?>"></textarea>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-secondary" data-bs-dismiss="modal">
+					<?= $ci->lang('cancel') ?>
+				</button>
+
+				<button class="btn btn-warning"
+						onclick="submitWithoutDatatable('init_lab_form', '<?= base_url() ?>admin/init_lab', 'labsTable', 'init_lab', print_lab ,'print'); list_labs();">
+					ذخیره و چاپ <i class="fa fa-print"></i>
+				</button>
+
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Modal Show End -->
